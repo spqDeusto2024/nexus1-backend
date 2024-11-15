689b95bac7c415d860ed1b71ae2356c95bb47982
Joxow <josu.igoa@opendeusto.es>
2024-11-15 16:41:33 +0100

    Created tenant endpoint

---
+++ app/endpoints/tenant.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends
+from app.controllers.tenant_handler import Tenant_Controller
+from app.auth.dependencies import get_current_user
+import app.models.models as models
+
+router = APIRouter()
+tenantController = Tenant_Controller()
+
+@router.get("/healthz")
+async def tenant_healthz(current_user: dict = Depends(get_current_user)):
+    return tenantController.healthz()
+
+@router.post('/create')
+async def create_tenant(body: models.TenantCreate,current_user: dict = Depends(get_current_user)):
+    return tenantController.create_tenant(body)
+
+@router.post('/delete')
+async def delete_tenant(body: models.TenantCreate,current_user: dict = Depends(get_current_user)):
+    return tenantController.delete_tenant(body)
+  
+@router.get('/get_all')
+async def get_all_tenant(current_user: dict = Depends(get_current_user)):
+    return tenantController.get_all()
+
+@router.post('/update')
+async def update_tenant(body: models.TenantUpdate,current_user: dict = Depends(get_current_user)):
+    return tenantController.update_tenant(body)
